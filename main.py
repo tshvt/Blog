5a269c035dc90c1d0b4b24dfb0fc187738c30d05
tshvt <ienar.k3@gmail.com>
2021-12-04 01:33:26 +0500

    Admin rights for user with id 1 and 5

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from functools import wraps
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY").replace("://", "ql://", 1)
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
